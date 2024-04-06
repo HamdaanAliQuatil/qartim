937a58af0f0bbebd9ef6d51c78d8e41f29e12061
lanixter <technix5622@gmail.com>
2024-04-07 00:12:15 +0530

    update .gitignore

--- send_key.py
+++ send_key.py
@@ -11,14 +11,7 @@ from bb84 import initiate_keygen, initialize_protocol, encode_qubits, array_to_s
 NUM_QUBITS = 32
 BACKEND = AerSimulator(method='stabilizer')
 
-# Initilisation of the encoding basis and the sender states of the quibits
+# Initilisation of the key
 key = initiate_keygen()
-
-# The bits to be sent
-sent_bits = array_to_string(sender_states)
-
-# Draw up a quantum circuit for encoding the qubit states
-encoded_qubits = encode_qubits(NUM_QUBITS, sender_states, encoding_basis)
-encoded_qubits.draw()
 
 
