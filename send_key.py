544f8c3f6ec3b2602786c1cfec71b72957525468
lanixter <technix5622@gmail.com>
2024-04-08 12:47:57 +0530

    update BB84.py

--- send_key.py
+++ send_key.py
@@ -5,6 +5,6 @@ from BB84 import initiate_keygen
 key = initiate_keygen(2)
 
 # save the key to a file
-with open('key.txt', 'w') as file:
-    file.write(key)
+with open('./key.txt', 'w') as file:
+    file.write(str(key))
 
